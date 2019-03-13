ef9aec7056fe67c6296671e07a20fd893bcdb4e6
PLopata <piotr.a.lopata@gmail.com>
2019-03-13 09:25:10 +0100

    laboratoria 2

--- Test/main.c
+++ Test/main.c
@@ -1,16 +1,26 @@
-int iPlusCounter;
-int iMinusCounter;
-int iLoopCtr;
+#include <LPC21xx.H>
 
+void delay()
+{
+	int i=0;
+	int a=0;
+	for(i=0; i<12000; i++)
+	{
+		a++;
+	}
+	
+}
 
 int main()
 {
-	iPlusCounter=10;
-	iMinusCounter=10;
-
-	for (iLoopCtr=0;iLoopCtr<5;iLoopCtr++)
-	{
-		iPlusCounter=iPlusCounter+3;
-		iMinusCounter=iMinusCounter-3;
-	}
+	IO1DIR=0x10000;
+	IO1SET=0x10000;
+	IO1CLR=0x10000;
+	while(1)
+		{
+		IO1SET=0x10000;
+		delay();
+		IO1CLR=0x10000;
+		delay();
+		}
 }
