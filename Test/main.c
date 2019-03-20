2097548fb381f9717c521956ba611dedfab08d81
PLopata <piotr.a.lopata@gmail.com>
2019-03-20 09:22:26 +0100

    Laboratoria 4, zadania 1-8

--- Test/main.c
+++ Test/main.c
@@ -1,26 +1,43 @@
 #include <LPC21xx.H>
 
-void delay()
+#define LEDOFF_bm = 0x00000
+#define LED0_bm 0x10000
+#define LED1_bm 0x20000
+#define LED2_bm 0x40000
+#define LED3_bm 0x80000
+#define LED03_bm 0xf0000
+
+void MsDelay(iMsNumber)
 {
-	int i=0;
-	int a=0;
-	for(i=0; i<12000; i++)
+	int iDelayIteration=0;
+	int iDelayVariable=0;
+	for(iDelayIteration=0; iDelayIteration<iMsNumber*4998; iDelayIteration++)
 	{
-		a++;
+		iDelayVariable++;
 	}
 	
 }
 
 int main()
 {
-	IO1DIR=0x10000;
-	IO1SET=0x10000;
-	IO1CLR=0x10000;
+	/*IO1DIR=LED03_bm;
+	IO1SET=LED0_bm;
+	IO1CLR=LED0_bm;
+	IO1SET=LED1_bm;
+	IO1CLR=LED1_bm;
+	IO1SET=LED2_bm;
+	IO1CLR=LED2_bm;
+	IO1SET=LED3_bm;
+	IO1CLR=LED3_bm;
+	IO1SET=LED03_bm;*/
+	
 	while(1)
 		{
-		IO1SET=0x10000;
-		delay();
-		IO1CLR=0x10000;
-		delay();
+			int LedNr;
+			IO1DIR=LED03_bm;
+			for(iLedNr=0; iLedNr<4; iLedNr++)
+			{
+				
+			}
 		}
 }
