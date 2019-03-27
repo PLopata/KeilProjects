9bfc16aa0b0608bec170d4700b5c0e39a93f6c4f
PLopata <piotr.a.alopata@gmail.com>
2019-03-27 09:13:23 +0100

    Labsnr4 i guess

--- Test/main.c
+++ Test/main.c
@@ -5,7 +5,9 @@
 #define LED1_bm 0x20000
 #define LED2_bm 0x40000
 #define LED3_bm 0x80000
-#define LED03_bm 0xf0000
+#define LED0123_bm 0x000f0000
+
+//current excercise: 16
 
 void MsDelay(iMsNumber)
 {
@@ -15,29 +17,76 @@ void MsDelay(iMsNumber)
 	{
 		iDelayVariable++;
 	}
-	
+}
+
+void LedInit()
+{
+	IO1DIR=(IO1DIR | LED0123_bm);
+	IO1CLR=LED0123_bm;
+	IO1SET=LED0_bm;
+}
+
+void LedOn(unsigned char ucLedIndeks)
+{
+	IO1CLR=LED0123_bm;
+	if(ucLedIndeks=='0') IO1SET=LED0_bm;
+	else if(ucLedIndeks=='1') IO1SET=LED1_bm;
+	else if(ucLedIndeks=='2') IO1SET=LED2_bm;
+	else if(ucLedIndeks=='3') IO1SET=LED3_bm;
+}
+
+int iReadButton1()
+{
+	IO0DIR=0x0;
+	IO0SET=0x10;
+	if(IO0PIN==0x10) return 1;
+	else return 0;
 }
 
 int main()
 {
-	/*IO1DIR=LED03_bm;
-	IO1SET=LED0_bm;
-	IO1CLR=LED0_bm;
-	IO1SET=LED1_bm;
-	IO1CLR=LED1_bm;
-	IO1SET=LED2_bm;
-	IO1CLR=LED2_bm;
-	IO1SET=LED3_bm;
-	IO1CLR=LED3_bm;
-	IO1SET=LED03_bm;*/
+	IO0DIR=0x0;
+	IO0SET=0x10;
+	IO1DIR=LED0123_bm;
+	
 	
 	while(1)
 		{
-			int LedNr;
-			IO1DIR=LED03_bm;
-			for(iLedNr=0; iLedNr<4; iLedNr++)
-			{
-				
-			}
+			//unsigned char iBut1State = iReadButton1();
+			if((IO0PIN & 0x10 )== 0) IO1SET=LED0_bm;
+			else IO1CLR=LED0123_bm;
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			/*
+			LedInit();
+			MsDelay(250);
+			LedOn('1');
+			MsDelay(250);
+			LedOn('2');
+			MsDelay(250);
+			LedOn('3');
+			MsDelay(250);
+			*/
+			
+			
+/*			LedInit();
+			MsDelay(250);
+			IO1CLR=LED0_bm;
+			IO1SET=LED1_bm;
+			MsDelay(250);
+			IO1CLR=LED1_bm;
+			IO1SET=LED2_bm;
+			MsDelay(250);
+			IO1CLR=LED2_bm;
+			IO1SET=LED3_bm;
+			MsDelay(250);
+			IO1CLR=LED3_bm;*/
 		}
 }
