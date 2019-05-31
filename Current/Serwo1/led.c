f0431cf041b4038de86f6247323c2bbd5ab43856
PLopata <piotr.a.lopata@gmail.com>
2019-05-31 12:54:06 +0200

    serwo 1

---
+++ Current/Serwo1/led.c
@@ -0,0 +1,69 @@
+#include <LPC21xx.H>
+#include "led.h"
+
+#define LED0_bm (1 << 16)
+#define LED1_bm (1 << 17)
+#define LED2_bm (1 << 18)
+#define LED3_bm (1 << 19)
+
+void LedInit()
+{
+	IO1DIR=(IO1DIR | LED0_bm | LED1_bm | LED2_bm |LED3_bm);
+	IO1CLR=(LED0_bm | LED1_bm | LED2_bm |LED3_bm);
+	IO1SET=LED0_bm;
+}
+
+void LedOn(unsigned char ucLedIndeks)
+{
+	IO1CLR=(LED0_bm | LED1_bm | LED2_bm |LED3_bm);
+	switch(ucLedIndeks)
+	{
+		case 0:
+			IO1SET=LED0_bm;
+			break;
+		case 1:
+			IO1SET=LED1_bm;
+			break;
+		case 2:
+			IO1SET=LED2_bm;
+			break;
+		case 3:
+			IO1SET=LED3_bm;
+	}
+}
+
+void LedLeft(void)
+{
+	static unsigned int uiLedPointer=0;
+	uiLedPointer=(uiLedPointer+1)%4;
+	LedOn(uiLedPointer);
+}
+
+
+enum Direction {LEFT, RIGHT};
+
+void LedStep(enum Direction eDirection)
+{
+	static unsigned int uiLedPointer=0; 
+	if( eDirection == LEFT)
+	{
+		uiLedPointer=(uiLedPointer+1)%4;
+		LedOn(uiLedPointer);
+	}
+	else
+	{
+		uiLedPointer=(uiLedPointer-1)%4;
+		LedOn(uiLedPointer);	
+	}
+}
+
+void LedStepLeft()
+{
+	LedStep(LEFT);
+}
+
+void LedStepRight()
+{
+	LedStep(RIGHT);
+}
+
---
+++ Current/Serwo1/main.c
@@ -0,0 +1,102 @@
+#include <LPC21xx.H>
+#include "led.h"
+#include "timer_interrupts.h"
+#include "keyboard.h"
+
+#define DETECTOR_bm (1<<10)
+
+void DetectorInit()
+{
+	IO0DIR = ( IO0DIR & (~DETECTOR_bm) );
+}
+
+enum DetectorState {INACTIVE, ACTIVE};
+
+enum DetectorState eReadDetector()
+{
+	if ( (IO0PIN & DETECTOR_bm) == 0)
+	{
+		return ACTIVE;
+	}
+	else 
+	{
+		return INACTIVE;
+	}
+}
+
+void Automat(void)
+{
+	static enum State {CALLIB, LED_LEFT, LED_RIGHT, WAIT} eLedState = CALLIB;
+
+	switch ( eLedState )
+	{
+		case CALLIB:
+		{
+			if ( eReadDetector() == INACTIVE )
+			{
+				eLedState = CALLIB;
+				LedStepLeft();
+			}
+			else 
+			{
+				eLedState = WAIT;
+			}
+			break;
+		}
+		case LED_LEFT:
+		{
+			if ( eReadButton() == BUTTON_1 )
+			{
+				eLedState = WAIT;
+			}
+			else 
+			{
+				eLedState = LED_LEFT;
+				LedStepLeft();
+			}
+			break;
+		}
+		case LED_RIGHT:
+		{
+			if ( eReadButton() == BUTTON_1 )
+			{
+				eLedState = WAIT;
+			}
+			else 
+			{
+				eLedState = LED_RIGHT;
+				LedStepRight();
+			}
+			break;
+		}
+		case WAIT:
+		{
+			if ( eReadButton() == BUTTON_0 )
+			{
+				eLedState = LED_LEFT;
+			}
+			else if ( eReadButton() == BUTTON_2 )
+			{
+				eLedState = LED_RIGHT;
+			}
+			else 
+			{
+				eLedState = WAIT;
+			}
+			break;
+		}
+	}
+}
+
+int main ()
+{
+	unsigned int iMainLoopCtr;
+	LedInit();
+	DetectorInit();
+	Timer0Interrupts_Init(10, &Automat );
+
+	while(1)
+	{
+		iMainLoopCtr++;
+	}
+}
